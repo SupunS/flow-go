96a6c0ed651cfd0652de7b9f581927ddf5713cdc
Janez Podhostnik <janez.podhostnik@gmail.com>
2021-01-19 16:25:40 +0100

    storage reporter creates a file

--- cmd/util/ledger/migrations/storage_reporter.go
+++ cmd/util/ledger/migrations/storage_reporter.go
@@ -1,7 +1,10 @@
 package migrations
 
 import (
+	"fmt"
 	"github.com/rs/zerolog"
+	"os"
+	"time"
 
 	"github.com/onflow/flow-go/ledger"
 	"github.com/onflow/flow-go/ledger/common/utils"
@@ -14,10 +17,25 @@ type StorageReporter struct {
 	Log zerolog.Logger
 }
 
+func filename() string {
+	return fmt.Sprintf("./storage_report_%d.csv", int32(time.Now().Unix()))
+}
+
 func (r StorageReporter) Report(payload []ledger.Payload) error {
-	r.Log.Info().Msg("Running Storage Reporter")
-	storageUsed := make(map[string]uint64)
-	isDapperAccount := make(map[string]bool)
+	fn := filename()
+	r.Log.Info().Msgf("Running Storage Reporter. Saving output to %s.", fn)
+
+	f, err := os.Create(fn)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
 
 	for _, p := range payload {
 		id, err := keyToRegisterID(p.Key)
@@ -35,14 +53,17 @@ func (r StorageReporter) Report(payload []ledger.Payload) error {
 		if err != nil {
 			return err
 		}
-		storageUsed[id.Owner] = u
-		isDapperAccount[id.Owner] = false
+		record := []byte(fmt.Sprintf("%s,%d,%t", id.Owner, u, false))
+		n, err := f.Write(record)
+		if err != nil {
+			return err
+		}
+		if n != len(record) {
+			return fmt.Errorf("failed to write data to %s", fn)
+		}
 	}
-	r.Log.Info().Msg("Storage Used")
-	for s, u := range storageUsed {
-		r.Log.Info().Msgf("%s,%u,%t", s,u, isDapperAccount[s])
-	}
-	r.Log.Info().Msg("End Of Storage Used")
+
+	r.Log.Info().Msg("Storage Reporter Done.")
 
 	return nil
 }
