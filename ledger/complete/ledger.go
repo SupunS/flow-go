503f57677fb76124d87f65f0cd91448f7f7be686
Janez Podhostnik <janez.podhostnik@gmail.com>
2021-01-19 16:32:45 +0100

    print tries

--- ledger/complete/ledger.go
+++ ledger/complete/ledger.go
@@ -269,9 +269,10 @@ func (l *Ledger) ExportCheckpointAt(state ledger.State,
 	// get trie
 	t, err := l.forest.GetTrie(ledger.RootHash(state))
 	if err != nil {
-		tries, err := l.forest.GetTries()
-		if err == nil {
-			l.logger.Warn().Msgf("Available tries: %v", tries)
+		l.logger.Info().Msgf("Available tries:")
+		tries, _ := l.forest.GetTries()
+		for _, t := range tries {
+			l.logger.Info().Msgf(">>>>", t.StringRootHash())
 		}
 		return nil, fmt.Errorf("cannot get try at the given state commitment: %w", err)
 	}
