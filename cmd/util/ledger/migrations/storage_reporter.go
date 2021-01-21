ab4a4b767bbf8e40f037ad9f79a23eec5b9532bf
Janez Podhostnik <janez.podhostnik@gmail.com>
2021-01-21 16:10:48 +0100

    refined the reporter

--- cmd/util/ledger/migrations/storage_reporter.go
+++ cmd/util/ledger/migrations/storage_reporter.go
@@ -3,6 +3,8 @@ package migrations
 import (
 	"bufio"
 	"fmt"
+	"github.com/onflow/cadence/runtime/common"
+	"github.com/onflow/cadence/runtime/interpreter"
 	"github.com/onflow/flow-go/fvm/state"
 	"github.com/rs/zerolog"
 	"os"
@@ -67,12 +69,17 @@ func (r StorageReporter) Report(payload []ledger.Payload) error {
 		if err != nil {
 			return err
 		}
+		balance, hasVault, err := r.balance(address, st)
+		if err != nil {
+			r.Log.Err(err).Msg("Cannot get account balance")
+			return err
+		}
 		dapper, err := r.isDapper(address, st)
 		if err != nil {
 			r.Log.Err(err).Msg("Cannot determine if this is a dapper account")
 			return err
 		}
-		record := fmt.Sprintf("%s,%d,%t\n", address.Hex(), u, dapper)
+		record := fmt.Sprintf("%s,%d,%d,%t,%t\n", address.Hex(), u, balance, hasVault, dapper)
 		_, err = writer.WriteString(record)
 		if err != nil {
 			return err
@@ -88,13 +95,65 @@ func (r StorageReporter) isDapper(address flow.Address, st *state.State) (bool, 
 	id := flow.RegisterID{
 		Owner:      string(address.Bytes()),
 		Controller: "",
-		Key:        fmt.Sprintf("%s\x1F%s", "storage", "flowTokenVault"),
+		Key:        fmt.Sprintf("%s\x1F%s", "public", "dapperUtilityCoinReceiver"),
 	}
 
 	resource, err := st.Get(id.Owner, id.Controller, id.Key)
 	if err != nil {
-		return false, fmt.Errorf("could not load storage capacity resource at %s: %w", id.String(), err)
+		return false, fmt.Errorf("could not load resource at %s: %w", address, err)
+	}
+	return len(resource)==0, nil
+}
+
+func (r StorageReporter) balance(address flow.Address, st *state.State) (balance uint64, hasBalance bool, err error) {
+	vaultId := flow.RegisterID{
+		Owner:      string(address.Bytes()),
+		Controller: "",
+		Key:        fmt.Sprintf("%s\x1F%s", "storage", "flowTokenVault"),
 	}
 
-	return len(resource) == 0, nil
+	balanceId := flow.RegisterID{
+		Owner:      string(address.Bytes()),
+		Controller: "",
+		Key:        fmt.Sprintf("%s\x1F%s", "public", "flowTokenBalance"),
+	}
+
+	balanceResource, err := st.Get(balanceId.Owner, balanceId.Controller, balanceId.Key)
+	if err != nil {
+		return 0, false, fmt.Errorf("could not load resource at %s: %w", address, err)
+	}
+
+	vaultResource, err := st.Get(vaultId.Owner, vaultId.Controller, vaultId.Key)
+	if err != nil {
+		return 0, false, fmt.Errorf("could not load resource at %s: %w", address, err)
+	}
+
+	if len(vaultResource) == 0 {
+		return 0, false, nil
+	}
+
+	if len(balanceResource) == 0 {
+		r.Log.Warn().Str("Account", address.HexWithPrefix()).Msgf("Address has a vault, but not a balance capability")
+	}
+
+	storedData, version := interpreter.StripMagic(vaultResource)
+	commonAddress := common.BytesToAddress([]byte(vaultId.Owner))
+	storedValue, err := interpreter.DecodeValue(storedData, &commonAddress, []string{vaultId.Key}, version)
+	if err != nil {
+		return 0, false, fmt.Errorf("could not decode resource at %s: %w", address, err)
+	}
+	composite, ok := storedValue.(*interpreter.CompositeValue)
+	if !ok {
+		return 0, false, fmt.Errorf("could not decode composite at %s: %w", address, err)
+	}
+	balanceField, ok := composite.Fields["balance"]
+	if !ok {
+		return 0, false, fmt.Errorf("could get balance field at %s: %w", address, err)
+	}
+	balanceValue, ok := balanceField.(interpreter.UFix64Value)
+	if !ok {
+		return 0, false, fmt.Errorf("could not decode resource at %s: %w", address, err)
+	}
+
+	return uint64(balanceValue), true, nil
 }
